25e31f1f2cc73d6b672417ee66303de669b5583f
ChungYin Wu <wuchu@flip3.engr.oregonstate.edu>
2019-05-18 19:04:34 -0700

    revised randomtestadventurer.c

--- projects/wuchu/dominion/randomtestadventurer.c
+++ projects/wuchu/dominion/randomtestadventurer.c
@@ -14,10 +14,10 @@ int main () {
 
    SelectStream(2);
    PutSeed(3);
-   //printf("still working 1\n");
+
    int n;
    for(n = 0; n < 1000; n++){
-      //printf("still working 2\n");      
+
       int drawntreasure = rand() % 2;
       int m;
       for(m = 0; m < sizeof(struct gameState); m++){
@@ -29,11 +29,10 @@ int main () {
 
       int oldhandcount = G.handCount[currentPlayer];     
       adventurerPlayed(drawntreasure, &G, currentPlayer, temphand, z);
-      if(G.handCount[currentPlayer] = oldhandcount + 2){
+      if(G.handCount[currentPlayer] == oldhandcount + 2){
          printf("Adventurer random test passed!\n");
       }else{
          printf("Adventurer random test failed!\n");
       }
    }
-
 }
