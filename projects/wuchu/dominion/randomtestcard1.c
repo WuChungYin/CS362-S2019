83e83caa2842b71a9664370a6da0b82252a16500
ChungYin Wu <wuchu@flip3.engr.oregonstate.edu>
2019-05-19 07:17:04 -0700

    added comments

--- projects/wuchu/dominion/randomtestcard1.c
+++ projects/wuchu/dominion/randomtestcard1.c
@@ -17,15 +17,17 @@ int main () {
 
    int n;
    for(n = 0; n < 1000; n++){
+//setting up random values to input into villagePlayed()
       int currentPlayer = rand() % 2;
       int m;
       for(m = 0; m < sizeof(struct gameState); m++){
          ((char*)&G)[m] = floor(rand() * 256);
       }
       int handPos = rand() % MAX_HAND;
-
+//storing initial value of numActions, should be increased by 2 after villagePlayed() is implemented
       int oldnumActions = G.numActions;
       villagePlayed(currentPlayer, &G, handPos);
+//checking if numActions has indeed been increased by 2
       if(G.numActions == oldnumActions + 2){
          printf("Village random test passed!\n");
       }else{
