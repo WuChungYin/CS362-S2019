3407dbab88774d03529050b3dc9597a2013266b0
ChungYin Wu <wuchu@flip3.engr.oregonstate.edu>
2019-05-19 07:17:32 -0700

    added comments

--- projects/wuchu/dominion/randomtestcard2.c
+++ projects/wuchu/dominion/randomtestcard2.c
@@ -17,15 +17,17 @@ int main () {
 
    int n;
    for(n = 0; n < 1000; n++){
+//setting up random input values for great_hallPlayed()
       int currentPlayer = rand() % 2;
       int m;
       for(m = 0; m < sizeof(struct gameState); m++){
          ((char*)&G)[m] = floor(rand() * 256);
       }
       int handPos = rand() % MAX_HAND;
-
+//storing initial value numActions, should be increased by 1 after great_hallPlayed() is implemented
       int oldnumActions = G.numActions;
       great_hallPlayed(currentPlayer, &G, handPos);
+//checking if numActions has indeed incremented by 1 after great_hallPlayed()
       if(G.numActions == oldnumActions + 1){
          printf("Great Hall random test passed!\n");
       }else{
